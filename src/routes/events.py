7ce135da769515e61f1cd08ef5c2e13ec5d68302
Byrone Kinsly <Kinslybyrone17@gmail.com>
2024-11-26 16:18:55 +0300

    added fetching of events, single events, creating event gallery and updated the booking logic

--- src/routes/events.py
+++ src/routes/events.py
@@ -1,12 +1,11 @@
 from datetime import datetime
-from re import match
 
-from fastapi import APIRouter, Depends, HTTPException, status, Body, Form, UploadFile
+from fastapi import APIRouter, Depends, HTTPException, status,  Form, UploadFile
 
-from src.models.schema import Activity, TestimonyCreate
+from src.models.schema import  TestimonyCreate
 from src.utils.cloudinary_config import upload_image
 from src.utils.config import get_current_user
-from typing import Annotated
+from typing import Annotated, List
 
 from src.db import prisma
 
@@ -119,7 +118,7 @@ async def get_images(event_id:str):
         raise HTTPException(status_code=400, detail=str(e))
 
 @router.get("/{event_id}/bookings")
-async def get_bookings(event_id:str,user = Depends(get_current_user())):
+async def get_bookings(event_id:str,user = Depends(get_current_user)):
     try:
         if not user:
             raise HTTPException(status_code=404, detail="User Not Found")
@@ -141,7 +140,7 @@ async def get_bookings(event_id:str,user = Depends(get_current_user())):
 
 
 @router.post("/{event_id}/testimony")
-async def create_testimony(event_id:str,testimony:TestimonyCreate,user= Depends(get_current_user())):
+async def create_testimony(event_id:str,testimony:TestimonyCreate,user= Depends(get_current_user)):
     try:
         if not user:
             raise HTTPException(status_code=404, detail="user Not Found")
@@ -170,3 +169,105 @@ async def create_testimony(event_id:str,testimony:TestimonyCreate,user= Depends(
     except Exception as e:
         raise HTTPException(status_code=400,detail=str(e))
 
+
+@router.post("/{event_id}/upload")
+async def upload_images(event_id:str,images:List[UploadFile],user= Depends(get_current_user)):
+    try:
+        if not user:
+            raise HTTPException(status_code=404, detail="user Not Found")
+        if user.role != "ADMIN":
+            raise HTTPException(status_code=401, detail="User Not Admin")
+
+        images_url = []
+
+        for image in images:
+            image_url = await upload_image(image)
+            if image_url:
+                images_url.append(image_url)
+                
+        images_upload = await prisma.activity.update(
+            where={
+                "id":event_id
+            },
+            data={
+               "images" :images_url
+            }
+
+        )
+        return images_upload
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+@router.put("/{event_id}")
+async def update_event(
+    event_id:str,
+    name: Annotated[str| None, Form()] = None,
+    location: Annotated[str | None, Form()] = None,
+    date: Annotated[str| None, Form()] = None,
+    price: Annotated[int| None, Form()] = None,
+    image: UploadFile | None = None,
+    discount: Annotated[int | None, Form()] = None,
+    max_attendees: Annotated[int | None, Form()] = None,
+    user=Depends(get_current_user),):
+
+
+    logo =None
+    newPrice = None
+    try:
+        if not user:
+            raise HTTPException(status_code=404,detail="User Not Found")
+
+        if discount and discount > 0:
+            newPrice = price - (price * discount / 100)
+        if date:
+            date = datetime.fromisoformat(date)
+
+        if image:
+            logo = await upload_image(image)
+
+            if not logo:
+                if not logo:
+                    raise HTTPException(status_code=400, detail="Failed to upload image")
+        data = {}
+        if name:
+            data["name"] = name
+        if location:
+            data["location"] = location
+        if date:
+            data["date"] = date
+        if price:
+            data["pricePerPerson"] = price
+        if discount:
+            data["discount"] = discount
+        if newPrice:
+            data["newPrice"] = newPrice
+        if max_attendees:
+            data["maxAttendees"] = max_attendees
+        if logo:
+            data["logo"] = logo
+
+        updated_event = await prisma.activity.update(
+            where={
+                "id":event_id
+            },
+            data=data
+        )
+        return updated_event
+    except Exception as e:
+        raise HTTPException(status_code=400,detail=str(e))
+
+@router.delete("/{event_id}")
+async def delete_event(event_id:str, user = Depends(get_current_user)):
+    try:
+        if not user:
+            raise HTTPException(status_code=404, detail="User Does Not Exist")
+        if user.role != "ADMIN":
+            raise HTTPException(status_code= 401, detail = "User Not Admin")
+        deleted_event = await prisma.activity.delete(
+            where={
+                "id":event_id
+            }
+        )
+        return deleted_event
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
